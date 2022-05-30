6cf01a75a13492a2fbdf311893b8569fbfa80feb
Michael Rubin <mhr@neverthere.org>
2022-05-29 21:46:31 -0700

    Initial checkin for VEB code.
    Mostly copied from anothr golang program.

---
+++ Makefile
@@ -0,0 +1,70 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+.PHONY: fix vet fmt test build tidy lint release
+
+default: build
+
+GOBIN := $(shell go env GOPATH)/bin
+REPO_GOLINT := github.com/golangci/golangci-lint/cmd/golangci-lint
+REPO_GOIMPORTS := golang.org/x/tools/cmd/goimports
+REPO_GODOC := golang.org/x/tools/cmd/godoc
+
+build:
+	go build -v
+
+cross:
+	env GOOS=linux   GOARCH=amd64 go build -v -o vebtree.linux
+	env GOOS=darwin  GOARCH=amd64 go build -v -o vebtree.darwin
+	env GOOS=windows GOARCH=amd64 go build -v -o vebtree.exe
+
+all: fix vet fmt tidy build lint test
+
+fix:
+	go fix ./...
+
+fmt:
+	go fmt ./...
+	go get $(REPO_GOIMPORTS)
+	$(GOBIN)/goimports -w .
+	gofmt -w -s .
+
+tidy:
+	go mod tidy
+
+test:
+	go test -timeout 60m ./...
+
+vet:
+	go vet ./...
+
+lint:
+	go get $(REPO_GOLINT)
+	$(GOBIN)/golangci-lint run ./...
+
+docs:
+	go get $(REPO_GODOC)
+	$(GOBIN)/godoc -http=localhost:6060
+
+covhtml:
+	go test ./... -coverprofile=cov.out
+	go tool cover -html=cov.out
+
+covfunc:
+	go test ./... -coverprofile=cov.out
+	go tool cover -func=cov.out
+
+clean:
+	rm -f vebtree cov.out *.bak *.json \
+		vebtree.darwin vebtree.exe vebtree.linux
---
+++ cmd/root.go
@@ -0,0 +1,74 @@
+/*
+Copyright © 2020 Michael Rubin <mhr@neverthere.org>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+func newVersionCmd() *cobra.Command {
+	const numVersionCmdArgs = 0
+
+	return &cobra.Command{
+		Use:   "version",
+		Short: "Output release version.",
+		Long:  `Outputs release version. `,
+		Args:  cobra.MaximumNArgs(numVersionCmdArgs),
+		Run: func(cmd *cobra.Command, args []string) {
+			version := "v1.1.2"
+			fmt.Printf("%s\n", version)
+		},
+	}
+}
+
+func newRootCmd() *cobra.Command {
+	// rootCmd represents the base command when called without any subcommands.
+	return &cobra.Command{
+		Use:   "vebtree",
+		Short: "Datastrucutre cmd",
+		Long: `Fun vebtree. `,
+	}
+}
+
+const exitErr = 1
+
+// Execute is the single function to set up the complete set of nested cobra
+// based commands that provide CLI functionality to the exifsort library.
+func Execute() {
+	cobra.OnInitialize(initConfig)
+
+	rootCmd := newRootCmd()
+/*
+	rootCmd.AddCommand(newEvalCmd())
+	rootCmd.AddCommand(newFilterCmd())
+	rootCmd.AddCommand(newMergeCmd())
+	rootCmd.AddCommand(newScanCmd())
+	rootCmd.AddCommand(newSortCmd())
+	rootCmd.AddCommand(newVersionCmd())
+*/
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(exitErr)
+	}
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+}
